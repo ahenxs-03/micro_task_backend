4c59e27569442312251c1a0252d25d216fd563fc
ahenxs-03 <bhattacharjeetapati0@gmail.com>
2025-12-10 15:30:50 +0000

    day1:=GET,POST

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #1 start server 
 from fastapi import FastAPI
 import uvicorn
-
+from pydantic import BaseModel
+class Item(BaseModel):
+    name:str
+    price:float
 # create app
 app=FastAPI() # FastAPI object 
 # home check:=
@@ -14,18 +17,35 @@ def home():
 @app.get('/health')
 def health():
     return {"status":"ok"}
-global id 
-id=1
+
 #2. POST /items--> 
 #    accepts JSON {"name":str,"price":float} and 
 #return same object plus id:<int>(auto increment)
+
+## concept ki baat
+## playload: type of input handle inputed data
+# 
+#-----XXXX---
+item_list=[]
+id_track=1
 @app.post('/items')
-def items():
+def items(playload:Item):
+    global id_track
+    global item_list
+    
+    d={"id":id_track,
+       "name":playload.name,
+       "price":playload.price
+    }
+    id_track+=1
 
-
-
-
-
+    item_list.append(d)
+    return d
+@app.get('/lis')
+def bill():
+    global item_list
+    return item_list 
+    
 # app run
 uvicorn.run(app)
 
